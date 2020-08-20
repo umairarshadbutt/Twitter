e616ad8a0c533abf57274d933f8694c7d20965ff
umairarshadbutt <umair6622@gmail.com>
2020-08-21 01:50:33 +0500

    create tweet form with simple_form_for, manage the columns in layout

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,6 +5,7 @@ class TweetsController < ApplicationController
   # GET /tweets.json
   def index
     @tweets = Tweet.all.order("created_at DESC")
+    @tweet = Tweet.new
   end
 
   # GET /tweets/1

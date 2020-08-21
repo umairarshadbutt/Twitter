9f020952355fd2aa2c57dfdf3da48aac6911ed36
umairarshadbutt <umair6622@gmail.com>
2020-08-21 22:28:11 +0500

    Rgistration controller added, Update device views

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,13 @@
+Class RegistrationsController < devise::RegistrationsController
+
+    private
+
+    def sign_up_params
+        params.require(:user).permimt(:name, :username, :email, :password, :password_confirmation)
+    end
+
+    def account_update_params
+        params.require(:user).permimt(:name, :username, :email, :password, :password_confirmation, :current_password)
+    end
+
+end
